5bae909505a2fc70a2557c37c51c57e6ccd9a3b7
Oleksii Honchar <tuiteraz@gmail.com>
2024-09-22 21:12:17 +0200

    feat: add server prod tweaks and admin creds;

--- scripts/check-realm.ts
+++ scripts/check-realm.ts
@@ -9,6 +9,7 @@ import pino from 'pino';
 const logger = pino({ level: 'info', name: 'check-realm' });
 
 import KcAdminClient from '@keycloak/keycloak-admin-client';
+import type { Credentials } from '@keycloak/keycloak-admin-client/lib/utils/auth.d.ts';
 
 async function waitForKeycloak(baseUrl: string, maxAttempts = 12, delay = 5000): Promise<void> {
   const url = new URL('/admin/master/console/', baseUrl);
@@ -39,72 +40,63 @@ async function waitForKeycloak(baseUrl: string, maxAttempts = 12, delay = 5000):
 const baseUrl = process.env.KEYCLOAK_BASE_URL || 'http://localhost:8080';
 await waitForKeycloak(baseUrl);
 
-const config = {
-  token: {
-    baseUrl,
-    username: 'olho',
-    password: 'olho',
-    grant_type: 'password',
-    client_id: 'react-app',
-    realmName: 'rnd',
-  },
-  adminClient: {
-    baseUrl,
-    realmName: 'master',
-    username: 'admin',
-    password: 'admin',
-    grantType: 'password',
-    clientId: 'admin-cli',
-  },
+const adminClientConfig = {
+  baseUrl,
+  realmName: 'master',
+  username: process.env.KEYCLOAK_ADMIN,
+  password: process.env.KEYCLOAK_ADMIN_PASSWORD,
+  grantType: 'password',
+  clientId: 'admin-cli',
 };
 
-const realmConfig = JSON.parse(readFileSync(new URL('./rnd-realm.json', import.meta.url), 'utf8'));
+const rndRealmConfig = JSON.parse(readFileSync(new URL('./rnd-realm.json', import.meta.url), 'utf8'));
+const masterRealmConfig = JSON.parse(readFileSync(new URL('./master-realm.json', import.meta.url), 'utf8'));
+
 // since SMTP-HOST stored globbaly in the system with port ¯\(ツ)/¯
 const smtpHost = process.env.SMTP_HOST ? process.env.SMTP_HOST.split(':')[0] : '';
 
 // updating config with sensitive info
-realmConfig.smtpServer.host = smtpHost;
-realmConfig.smtpServer.user = process.env.SMTP_USER;
-realmConfig.smtpServer.password = process.env.SMTP_PASSWORD;
-realmConfig.smtpServer.replyTo = process.env.SMTP_REPLY_TO;
-realmConfig.smtpServer.from = process.env.SMTP_REPLY_TO;
-realmConfig.users[1].email = process.env.ADMIN_EMAIL;
-realmConfig.attributes['frontendUrl'] = process.env.KEYCLOAK_FRONTEND_URL;
+rndRealmConfig.smtpServer.host = smtpHost;
+rndRealmConfig.smtpServer.user = process.env.SMTP_USER;
+rndRealmConfig.smtpServer.password = process.env.SMTP_PASSWORD;
+rndRealmConfig.smtpServer.replyTo = process.env.SMTP_REPLY_TO;
+rndRealmConfig.smtpServer.from = process.env.SMTP_REPLY_TO;
+rndRealmConfig.attributes['frontendUrl'] = process.env.KEYCLOAK_FRONTEND_URL;
 
-const adminClient = new KcAdminClient(config.adminClient);
-await adminClient.auth(config.adminClient);
+masterRealmConfig.smtpServer.host = smtpHost;
+masterRealmConfig.smtpServer.user = process.env.SMTP_USER;
+masterRealmConfig.smtpServer.password = process.env.SMTP_PASSWORD;
+masterRealmConfig.smtpServer.replyTo = process.env.SMTP_REPLY_TO;
+masterRealmConfig.smtpServer.from = process.env.SMTP_REPLY_TO;
+masterRealmConfig.attributes['frontendUrl'] = process.env.KEYCLOAK_FRONTEND_URL;
+
+const adminClient = new KcAdminClient(adminClientConfig);
+await adminClient.auth(adminClientConfig as Credentials);
 
 // creating realm
-await adminClient.realms.create(realmConfig);
+await adminClient.realms.create(rndRealmConfig);
 
 // Update admin email
-const users = await adminClient.users.find({ realm: config.adminClient.realmName });
-const adminUser = users.find(user => user.username === 'admin');
+const users = await adminClient.users.find({ realm: adminClientConfig.realmName });
+const adminUser = users.find(user => user.username === process.env.KEYCLOAK_ADMIN);
+
+logger.info({ adminUser }, 'Updating admin email ...');
+
 if (adminUser) {
   await adminClient.users.update(
-    { id: adminUser.id, realm: config.adminClient.realmName },
-    { email: process.env.KEYCLOAK_ADMIN_EMAIL || 'default_admin@example.com' }
-  );
-
-  await adminClient.users.resetPassword({
-    id: adminUser.id,
-    realm: config.adminClient.realmName,
-    credential: {
-      type: 'password',
-      value: process.env.KEYCLOAK_ADMIN_PASSWORD || 'default_password',
-      temporary: false,
+    { id: adminUser.id, realm: adminClientConfig.realmName },
+    {
+      email: process.env.KEYCLOAK_ADMIN_EMAIL || 'default_admin@example.com',
+      emailVerified: true
     }
-  });
+  );
 }
-logger.info(`Realm ${realmConfig.realm} created successfully.`);
+logger.info(`Realm ${rndRealmConfig.realm} created successfully.`);
 
-logger.info('Updating master realm frontendUrl...');
+logger.info('Updating master realm ...');
 const masterRealm = await adminClient.realms.find({ realm: 'master' });
 if (masterRealm) {
-  await adminClient.realms.update(
-    { realm: 'master' },
-    { attributes: { 'frontendUrl': process.env.KEYCLOAK_FRONTEND_URL } }
-  );
+  await adminClient.realms.update({ realm: 'master' }, masterRealmConfig);
 }
-logger.info('Master realm frontendUrl updated successfully.');
+logger.info('Master realm updated successfully.');
 process.exit(0);
